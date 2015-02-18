41371421386861a8ab45de4fbba6a33d2c4b70b8
Milan Panchal <MilanPanchal@PA-MAC-2.local>
2015-02-18 11:33:49 +0530

    Initial Commit

---
+++ FbLoginExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FbLoginExample
+//
+//  Created by Milan Panchal on 2/18/15.
+//  Copyright (c) 2015 Pantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FbLoginExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FbLoginExample
+//
+//  Created by Milan Panchal on 2/18/15.
+//  Copyright (c) 2015 Pantech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
